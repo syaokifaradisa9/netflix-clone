a6eae06f60bfe9a95b8870c6667ea4d81a273e96
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2024-07-23 11:49:07 +0800

    fix: fix jumbotron styling

--- frontend/src/components/Jumbotron/index.jsx
+++ frontend/src/components/Jumbotron/index.jsx
@@ -13,12 +13,12 @@ const Jumbotron = () => {
             <img
                 src={JUMBOTRON_IMAGE}
                 alt='Netflix-bg'
-                className='absolute top-0 left-0 object-cover h-[700px] w-full'
+                className='absolute top-0 left-0 object-cover h-[700px] w-full opacity-60'
             />
             <EachUtils
                 of={language == "id" ? LIST_JUMBOTRON_ID : LIST_JUMBOTRON_EN}
                 render={(item, index) => (
-                    <div className='relative flex flex-col justify-center items-center mt-44 gap-4 text-center'>
+                    <div className='relative flex flex-col justify-center items-center mt-44 gap-4 text-center px-4'>
                         <h1
                             className='font-black text-white text-4xl'>
                             {item.title}
